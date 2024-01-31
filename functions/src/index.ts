6687069ddc8fdf94286f04dcf0ef650be5ced58b
@b1scuit <dysvir@gmail.com>
2024-01-31 10:55:43 +0000

    Updates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,27 +5,27 @@
  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
-*/ 
+*/
 
-import { onCall} from "firebase-functions/v2/https";
-const fetch = require('node-fetch')
-const FormData = require('form-data');
+import {onCall} from "firebase-functions/v2/https";
 
+const fetch = require("node-fetch");
+const FormStuff = require("form-data");
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 export const tokenExchange = onCall((request) => {
-    const body = new FormData()
-    body.append("grant_type", "authorization_code")
-    body.append("client_id", process.env.CLIENT_ID)
-    body.append("client_secret", process.env.CLIENT_SECRET)
-    body.append("redirect_uri", process.env.REDIRECT_URI)
-    body.append("code", request.data.code)
+  const body = new FormStuff();
+  body.append("grant_type", "authorization_code");
+  body.append("client_id", process.env.CLIENT_ID);
+  body.append("client_secret", process.env.CLIENT_SECRET);
+  body.append("redirect_uri", process.env.REDIRECT_URI);
+  body.append("code", request.data.code);
 
-    const fetchFromUrl = async () => await (await fetch("https://api.monzo.com/oauth2/token", {
-        method: "post",
-        body: body
-    })).json()
+  const fetchFromUrl = async () => await (await fetch("https://api.monzo.com/oauth2/token", {
+    method: "post",
+    body: body,
+  })).json();
 
-    return fetchFromUrl().then((data) => data)
-})
+  return fetchFromUrl().then((data) => data);
+});
